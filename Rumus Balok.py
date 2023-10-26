35b50fd005a76dce1dc7e3c4fc681dbabfc99224
RidwanNurjayadi <148680382+RidwanNurjayadi@users.noreply.github.com>
2023-10-27 01:26:46 +0700

    Add files via upload

---
+++ Rumus Balok.py
@@ -0,0 +1,29 @@
+print("Aplikasi Menghitung Luas dan Volume Balok")
+
+"""
+Nama    : Ridwan Nurjayadi
+NIM     : 220511039
+Kelas   : TI22L
+PBO     : Tugas Pertemuan 1
+
+Keterangan:
+p = Panjang
+l = Lebar
+t = Tinggi
+"""
+
+# Atur Nilai
+p = 10
+l = 9
+t = 14
+
+# Rumus
+luas = (2 * p * l) + (2  * p * t)
+volume = (p * l * t)
+
+# Output
+print("Panjang:", p)
+print("Lebar:", l)
+print("Tinggi:", t)
+print("Luas:", luas)
+print("Volume:", volume)
